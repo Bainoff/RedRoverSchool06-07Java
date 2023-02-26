122685a68f35c0010102823439fe0bceb7a3ba2f
Alexey <bainoff@gmail.com>
2023-02-26 23:33:08 +0100

    homework 2

--- src/homework/HW1.java
+++ src/homework/HW1.java
@@ -1,12 +1,13 @@
 package homework;
 
-public class Homework1 {
+public class HW1 {
     public static void main(String[] args) {
         System.out.println(3);
         System.out.println(2);
         System.out.println(1);
         System.out.println("Let's go!!!");
 
+        //TODO extra
         System.out.println("____888888______________________________\n" +
                 "____888888______________________________\n" +
                 "___8888888888___________________________\n" +
--- src/homework/HW2.java
+++ src/homework/HW2.java
@@ -1,2 +1,20 @@
-package homework;public class HW2 {
+package homework;
+
+public class HW2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+
+        //TODO extra
+        System.out.println(a % b);
+        System.out.println(a%2==0);
+        System.out.println(b%2==0);
+        System.out.println("\u263a");
+
+    }
+
 }
